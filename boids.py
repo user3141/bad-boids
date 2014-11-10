59c426dabf2cd441580ae89f081a6acdb240df47
tv <tv@localhost.localdomain>
2014-11-10 23:07:05 +0000

    Replaced magic numbers.

--- boids.py
+++ boids.py
@@ -9,10 +9,16 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-boids_x = [random.uniform(-450, 50.0) for x in range(50)]
-boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
-boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
-boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
+num_boids = 50
+neighbor_dist_cutoff = 100
+neighbor_vel_dist_cutoff = 10000
+center_damping =  0.01
+vel_damping = 0.125
+
+boids_x = [random.uniform(-450, 50.0) for x in range(num_boids)]
+boids_y = [random.uniform(300.0, 600.0) for x in range(num_boids)]
+boid_x_velocities = [random.uniform(0, 10.0) for x in range(num_boids)]
+boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(num_boids)]
 boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
 def update_boids(boids):
@@ -20,20 +26,20 @@ def update_boids(boids):
 	# Fly towards the middle
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			xvs[i] = xvs[i] + (xs[j] - xs[i]) * 0.01 / len(xs)
-			yvs[i] = yvs[i] + (ys[j] - ys[i]) * 0.01 / len(xs)
+			xvs[i] = xvs[i] + (xs[j] - xs[i]) * center_damping / len(xs)
+			yvs[i] = yvs[i] + (ys[j] - ys[i]) * center_damping / len(xs)
 	# Fly away from nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < 100:
+			if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_dist_cutoff:
 				xvs[i] = xvs[i] + (xs[i] - xs[j])
 				yvs[i] = yvs[i] + (ys[i] - ys[j])
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < 10000:
-				xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * 0.125 / len(xs)
-				yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
+			if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_vel_dist_cutoff:
+				xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * vel_damping / len(xs)
+				yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * vel_damping / len(xs)
 	# Move according to velocities
 	for i in range(len(xs)):
 		xs[i] = xs[i] + xvs[i]
