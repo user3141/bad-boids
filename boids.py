bb355ff733e470bd3e5678d465d622f3b6b5c176
tv <tv@localhost.localdomain>
2014-11-11 16:56:39 +0000

    Attempt to vectorize

--- boids.py
+++ boids.py
@@ -17,58 +17,91 @@ center_damping = 0.01
 vel_matching_strength = 0.125
 
 # initialisation
-boids_x = np.array([random.uniform(-450, 50.0) for x in range(num_boids)])
-boids_y = np.array([random.uniform(300.0, 600.0) for x in range(num_boids)])
-boid_x_velocities = np.array([random.uniform(0, 10.0) for x in range(num_boids)])
-boid_y_velocities = np.array([random.uniform(-20.0, 20.0) for x in range(num_boids)])
-boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+boids_pos = [np.array([random.uniform(-450, 50.0), random.uniform(300.0, 600.0)]) for _ in range(num_boids)]
+boids_vel = [np.array([random.uniform(   0, 10.0), random.uniform(-20.0,  20.0)]) for _ in range(num_boids)]
+boids = (boids_pos, boids_vel)
 
 
-def fly_towards_centre(xs, ys, xvs, yvs, center_damping):
+def fly_towards_centre(boids_pos, boids_vel, center_damping):
     """Boids fly towards the center of all other boids."""
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            xvs[i] = xvs[i] + (xs[j] - xs[i]) * center_damping / len(xs)
-            yvs[i] = yvs[i] + (ys[j] - ys[i]) * center_damping / len(xs)
+    new_boids_vel = []
+    for boid1_pos, boid_vel in zip(boids_pos, boids_vel):
+        for boid2_pos in boids_pos:
+            boid_vel += (boid2_pos - boid1_pos) * center_damping / len(boids_pos)
+
+        new_boids_vel.append(boid_vel)
+    boids_vel[0:] = [new_boid_vel for new_boid_vel in new_boids_vel]
+    #for i in range(len(xs)):
+    #    for j in range(len(xs)):
+    #        xvs[i] = xvs[i] + (xs[j] - xs[i]) * center_damping / len(xs)
+    #        yvs[i] = yvs[i] + (ys[j] - ys[i]) * center_damping / len(xs)
 
 
-def avoid_nearby_boids(xs, ys, xvs, yvs, neighbor_dist_cutoff):
+def distance(vec1, vec2):
+    """Distance between two vectors"""
+    diff_vec = vec1 - vec2
+    return np.sqrt(np.sum(x_i**2 for x_i in diff_vec))
+
+
+def avoid_nearby_boids(boids_pos, boids_vel, neighbor_dist_cutoff):
     """Keep some distance to nearby boids."""
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_dist_cutoff:
-                xvs[i] = xvs[i] + (xs[i] - xs[j])
-                yvs[i] = yvs[i] + (ys[i] - ys[j])
+    new_boids_vel = []
+    for boid1_pos, boid_vel in zip(boids_pos, boids_vel):
+        for boid2_pos in boids_pos:
+            if distance(boid1_pos, boid2_pos) < np.sqrt(neighbor_dist_cutoff):
+                boid_vel += (boid1_pos - boid2_pos)
+
+        new_boids_vel.append(boid_vel)
+    boids_vel[0:] = [new_boid_vel for new_boid_vel in new_boids_vel]
+    #for i in range(len(xs)):
+    #    for j in range(len(xs)):
+    #        if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_dist_cutoff:
+    #            xvs[i] = xvs[i] + (xs[i] - xs[j])
+    #            yvs[i] = yvs[i] + (ys[i] - ys[j])
 
 
-def adapt_speed(xs, ys, xvs, yvs, neighbor_vel_dist_cutoff):
+def match_speed(boids_pos, boids_vel, neighbor_vel_dist_cutoff):
     """Try to match speed with nearby boids"""
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_vel_dist_cutoff:
-                xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * vel_matching_strength / len(xs)
-                yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * vel_matching_strength / len(xs)
+    new_boids_vel = []
+    for boid1_pos, boid1_vel in zip(boids_pos, boids_vel):
+        for boid2_pos, boid2_vel in zip(boids_pos, boids_vel):
+            if distance(boid1_pos, boid2_pos) < np.sqrt(neighbor_vel_dist_cutoff):
+                boid1_vel += (boid2_vel - boid1_vel) * vel_matching_strength / len(boids_pos)
+
+        new_boids_vel.append(boid1_vel)
+    boids_vel[0:] = [new_boid_vel for new_boid_vel in new_boids_vel]
+    #for i in range(len(xs)):
+    #    for j in range(len(xs)):
+    #        if (xs[j] - xs[i])**2 + (ys[j] - ys[i])**2 < neighbor_vel_dist_cutoff:
+    #            xvs[i] = xvs[i] + (xvs[j] - xvs[i]) * vel_matching_strength / len(xs)
+    #            yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * vel_matching_strength / len(xs)
 
 
 def update_boids(boids):
-    xs, ys, xvs, yvs = boids
-    fly_towards_centre(xs, ys, xvs, yvs, center_damping)
-    avoid_nearby_boids(xs, ys, xvs, yvs, neighbor_dist_cutoff)
-    adapt_speed(xs, ys, xvs, yvs, neighbor_vel_dist_cutoff)
+    boids_pos, boids_vel = boids
+    fly_towards_centre(boids_pos, boids_vel, center_damping)
+    avoid_nearby_boids(boids_pos, boids_vel, neighbor_dist_cutoff)
+    match_speed(boids_pos, boids_vel, neighbor_vel_dist_cutoff)
 
-    for i in range(len(xs)):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
+    new_boids_pos = []
+    for boid_pos, boid_vel in zip(boids_pos, boids_vel):
+        boid_pos = boid_pos + boid_vel
+        new_boids_pos.append(boid_pos)
+    boids_pos[0:] = [boid_pos for boid_pos in new_boids_pos]
+
+def animate(frame):
+    update_boids(boids)
+    boids_pos, boids_vel = boids
+    x_vals = [x[0] for x in boids_pos]
+    y_vals = [x[1] for x in boids_pos]
+    scatter.set_offsets(zip(x_vals, y_vals))
 
 
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
-
-def animate(frame):
-    update_boids(boids)
-    scatter.set_offsets(zip(boids[0], boids[1]))
-
+x_vals = [x[0] for x in boids_pos]
+y_vals = [x[1] for x in boids_pos]
+scatter = axes.scatter(x_vals, y_vals)
 
 anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
