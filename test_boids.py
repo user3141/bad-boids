a67b9a672a129298b9a51f59198440ad0b8e4f59
tv <tv@localhost.localdomain>
2014-11-11 17:19:59 +0000

    Modified test case to match the new data structure.

--- test_boids.py
+++ test_boids.py
@@ -1,13 +1,25 @@
-from boids import update_boids
+from boids import update_boids, fly_towards_centre
 from nose.tools import assert_almost_equal
 import os
 import yaml
+import numpy as np
 
 def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
-    update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
-	
+
+    def convert_data(boid_data):
+        x, y, velx, vely = boid_data
+        boids_pos = [np.array([x_i, y_i]) for x_i, y_i in zip(x, y)]
+        boids_vel = [np.array([x_i, y_i]) for x_i, y_i in zip(velx, vely)]
+        boid_data = (boids_pos, boids_vel)
+        return boid_data
+
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+    boid_data_before = convert_data(regression_data["before"])
+    boid_data_after = convert_data(regression_data["after"])
+
+    update_boids(boid_data_before)
+    for list_after, list_before in zip(boid_data_after, boid_data_before):
+        for vec_after, vec_before in zip(list_after, list_before):
+            for val_after, val_before in zip(vec_after, vec_before):
+                assert_almost_equal(val_after, val_before, delta=0.01)
+
