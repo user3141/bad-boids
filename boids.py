7b73222183cfe016b38b7a693e0bd2a02d18f1a7
tv <tv@localhost.localdomain>
2014-11-17 21:38:33 +0000

    Split Boid class into Starling and Eagle and adapted test cases. Todo: inherite from abstract Boid class.

--- boids.py
+++ boids.py
@@ -8,14 +8,58 @@ from numpy import array
 
 # Will now add an Eagle to Boids
 
+#class Boid(object):
+#    def __init__(self, x, y, xv, yv, owner, species="Starling"):
+#        self.position = array([x, y])
+#        self.velocity = array([xv, yv])
+#        self.owner = owner
+#        self.species = species
+#
+#    def interaction(self, other):
+#        delta_v = array([0.0, 0.0])
+#        separation = other.position - self.position
+#        separation_sq = separation.dot(separation)
+#
+#        if other.species == "Eagle":
+#            # Flee the Eagle
+#            if separation_sq < self.owner.eagle_avoidance_radius**2:
+#                delta_v -= (separation * self.owner.eagle_fear) / separation.dot(separation)
+#                return delta_v
+#
+#        if self.species == "Eagle":
+#            # Hunt the boids
+#            delta_v += separation * self.owner.eagle_hunt_strength
+#        else:
+#            # Fly towards the middle
+#            delta_v += separation * self.owner.flock_attraction
+#
+#            # Fly away from nearby boids
+#            if separation_sq < self.owner.avoidance_radius**2:
+#                delta_v -= separation
+#
+#            # Try to match speed with nearby boids
+#            if separation_sq < self.owner.formation_flying_radius**2:
+#                delta_v += (other.velocity - self.velocity) * self.owner.speed_matching_strength
+#
+#        return delta_v
+
+
 class Boid(object):
-    def __init__(self, x, y, xv, yv, owner, species="Starling"):
+    def __init__(self, x, y, xv, yv, owner, species):
         self.position = array([x, y])
         self.velocity = array([xv, yv])
         self.owner = owner
         self.species = species
 
-    def interaction(self,other):
+
+class Starling(Boid):
+    def __init__(self, x, y, xv, yv, owner, species='Starling'):
+        self.position = array([x, y])
+        self.velocity = array([xv, yv])
+        self.owner = owner
+        self.species = species
+
+    def interaction(self, other):
         delta_v = array([0.0, 0.0])
         separation = other.position - self.position
         separation_sq = separation.dot(separation)
@@ -26,30 +70,50 @@ class Boid(object):
                 delta_v -= (separation * self.owner.eagle_fear) / separation.dot(separation)
                 return delta_v
 
-        if self.species == "Eagle":
-            # Hunt the boids
-            delta_v += separation * self.owner.eagle_hunt_strength
-        else:
-            # Fly towards the middle
-            delta_v += separation * self.owner.flock_attraction
+        # Fly towards the middle
+        delta_v += separation * self.owner.flock_attraction
 
-            # Fly away from nearby boids
-            if separation_sq < self.owner.avoidance_radius**2:
-                delta_v -= separation
+        # Fly away from nearby boids
+        if separation_sq < self.owner.avoidance_radius**2:
+            delta_v -= separation
 
-            # Try to match speed with nearby boids
-            if separation_sq < self.owner.formation_flying_radius**2:
-                delta_v += (other.velocity - self.velocity) * self.owner.speed_matching_strength
+        # Try to match speed with nearby boids
+        if separation_sq < self.owner.formation_flying_radius**2:
+            delta_v += (other.velocity - self.velocity) * self.owner.speed_matching_strength
+
+        return delta_v
+
+
+class Eagle(Boid):
+    def __init__(self, x, y, xv, yv, owner, species):
+        self.position = array([x, y])
+        self.velocity = array([xv, yv])
+        self.owner = owner
+        self.species = species
+
+    def interaction(self, other):
+        delta_v = array([0.0, 0.0])
+        separation = other.position - self.position
+        separation_sq = separation.dot(separation)
+
+        if other.species == "Eagle":
+            # Flee the other Eagle(s)
+            if separation_sq < self.owner.eagle_avoidance_radius**2:
+                delta_v -= (separation * self.owner.eagle_fear) / separation.dot(separation)
+                return delta_v
+
+        # Hunt the boids
+        delta_v += separation * self.owner.eagle_hunt_strength
 
         return delta_v
 
 
 # Deliberately terrible code for teaching purposes
 class Boids(object):
-    def __init__(self,
-            flock_attraction, avoidance_radius,
-            formation_flying_radius, speed_matching_strength,
-            eagle_avoidance_radius=100, eagle_fear=5000, eagle_hunt_strength=0.00005):
+    def __init__(self, flock_attraction, avoidance_radius,
+                 formation_flying_radius, speed_matching_strength,
+                 eagle_avoidance_radius=100, eagle_fear=5000,
+                 eagle_hunt_strength=0.00005):
         self.flock_attraction = flock_attraction
         self.avoidance_radius = avoidance_radius
         self.formation_flying_radius = formation_flying_radius
@@ -59,17 +123,17 @@ class Boids(object):
         self.eagle_hunt_strength = eagle_hunt_strength
 
 
-    def initialise_random(self,count):
-        self.boids = [Boid(random.uniform(-450, 50.0),
-                random.uniform(300.0, 600.0),
-                random.uniform(0, 10.0),
-                random.uniform(-20.0, 20.0),self) for i in range(count)]
+    def initialise_random(self, count):
+        self.boids = [Starling(random.uniform(-450, 50.0),
+                     random.uniform(300.0, 600.0),
+                     random.uniform(0, 10.0),
+                     random.uniform(-20.0, 20.0), self, species='Starling') for i in range(count)]
 
     def add_eagle(self, x, y, xv, yv):
-        self.boids.append(Boid(x, y, xv, yv, self, species="Eagle"))
+        self.boids.append(Eagle(x, y, xv, yv, self, species='Eagle'))
 
     def initialise_from_data(self, data):
-        self.boids = [Boid(x, y, xv, yv, self) for x, y, xv, yv in zip(*data)]
+        self.boids = [Starling(x, y, xv, yv, self, species='Starling') for x, y, xv, yv in zip(*data)]
 
     def update(self):
         for me in self.boids:
--- test_boids.py
+++ test_boids.py
@@ -1,50 +1,50 @@
 import boids as bd
-from nose.tools import assert_almost_equal, assert_greater
-from nose.tools import assert_less, assert_equal, assert_sequence_equal
+from nose.tools import assert_almost_equal,  assert_greater
+from nose.tools import assert_less,  assert_equal,  assert_sequence_equal
 from numpy.testing import assert_array_equal
 import os
 import yaml
 
 def test_bad_boids_regression():
-    boids=bd.Boids(0.01/50,10,100,0.125/50)
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    boids = bd.Boids(0.01/50, 10, 100, 0.125/50)
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
     boids.initialise_from_data(regression_data["before"])
     boids.update()
-    for index,boid in enumerate(boids.boids):
-        assert_almost_equal(boid.position[0],regression_data["after"][0][index],delta=0.01)
-        assert_almost_equal(boid.position[1],regression_data["after"][1][index],delta=0.01)
-        assert_almost_equal(boid.velocity[0],regression_data["after"][2][index],delta=0.01)
-        assert_almost_equal(boid.velocity[1],regression_data["after"][3][index],delta=0.01)
+    for index, boid in enumerate(boids.boids):
+        assert_almost_equal(boid.position[0], regression_data["after"][0][index], delta=0.01)
+        assert_almost_equal(boid.position[1], regression_data["after"][1][index], delta=0.01)
+        assert_almost_equal(boid.velocity[0], regression_data["after"][2][index], delta=0.01)
+        assert_almost_equal(boid.velocity[1], regression_data["after"][3][index], delta=0.01)
 
-	
+
 def test_bad_boids_initialisation():
-    boids=bd.Boids(1.0,10.0,100.0,0.5)
+    boids = bd.Boids(1.0, 10.0, 100.0, 0.5)
     boids.initialise_random(15)
-    assert_equal(len(boids.boids),15)
+    assert_equal(len(boids.boids), 15)
     for boid in boids.boids:
-        assert_less(boid.position[0],50.0)
-        assert_greater(boid.position[0],-450)
-        assert_less(boid.position[1],600)
-        assert_greater(boid.position[1],300)
-        assert_less(boid.velocity[0],10.0)
-        assert_greater(boid.velocity[0],0)
-        assert_less(boid.velocity[1],20.0)
-        assert_greater(boid.velocity[1],-20.0)
+        assert_less(boid.position[0], 50.0)
+        assert_greater(boid.position[0], -450)
+        assert_less(boid.position[1], 600)
+        assert_greater(boid.position[1], 300)
+        assert_less(boid.velocity[0], 10.0)
+        assert_greater(boid.velocity[0], 0)
+        assert_less(boid.velocity[1], 20.0)
+        assert_greater(boid.velocity[1], -20.0)
 
 def test_boid_interaction_fly_to_middle():
-    boids=bd.Boids(3.0,2.0,10,0)
-    first=bd.Boid(0,0,1,0,boids)
-    second=bd.Boid(0,5,0,0,boids)
-    assert_array_equal(first.interaction(second),[0.0,15.0])
+    boids = bd.Boids(3.0, 2.0, 10, 0)
+    first = bd.Starling(0, 0, 1, 0, boids)
+    second = bd.Starling(0, 5, 0, 0, boids)
+    assert_array_equal(first.interaction(second), [0.0, 15.0])
 
 def test_boid_interaction_avoidance():
-    boids=bd.Boids(3.0,10.0,10,0)
-    first=bd.Boid(0,0,1,0,boids)
-    second=bd.Boid(0,5,0,0,boids)
-    assert_array_equal(first.interaction(second),[0.0,10.0])
+    boids = bd.Boids(3.0, 10.0, 10, 0)
+    first = bd.Starling(0, 0, 1, 0, boids)
+    second = bd.Starling(0, 5, 0, 0, boids)
+    assert_array_equal(first.interaction(second), [0.0, 10.0])
 
 def test_boid_interaction_formation():
-    boids=bd.Boids(3.0,2.0,10.0,7.0)
-    first=bd.Boid(0,0,0.0,0,boids)
-    second=bd.Boid(0,5,11.0,0,boids)
-    assert_array_equal(first.interaction(second),[11.0*7.0,15.0])
+    boids = bd.Boids(3.0, 2.0, 10.0, 7.0)
+    first = bd.Starling(0, 0, 0.0, 0, boids)
+    second = bd.Starling(0, 5, 11.0, 0, boids)
+    assert_array_equal(first.interaction(second), [11.0*7.0, 15.0])
