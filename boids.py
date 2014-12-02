b943b7ab03e3650aeb11094143c732f19f51002c
tv <tv@localhost.localdomain>
2014-12-02 15:52:01 +0000

    Using numpy arrays made it much slower. 500 runs: 17.4 s

--- boids.py
+++ boids.py
@@ -6,14 +6,15 @@ for use as an exercise on refactoring.
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
+import numpy as np
 
 # Deliberately terrible code for teaching purposes
 
-boids_x = [random.uniform(-450, 50.0) for x in range(50)]
-boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
-boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
-boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
-boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+boids_x           = np.array([random.uniform(-450.,  50.0) for x in range(50)])
+boids_y           = np.array([random.uniform( 300., 600.0) for x in range(50)])
+boid_x_velocities = np.array([random.uniform(   0.,  10.0) for x in range(50)])
+boid_y_velocities = np.array([random.uniform(-20.0,  20.0) for x in range(50)])
+boids = [boids_x, boids_y, boid_x_velocities, boid_y_velocities]
 
 def update_boids(boids):
     xs, ys, xvs, yvs = boids
@@ -38,9 +39,8 @@ def update_boids(boids):
                 yvs[i] = yvs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
     # Move according to velocities
     for i in range(len(xs)):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
-
+        xs[i] = xs [i] + xvs[i]
+        ys[i] = ys [i] + yvs[i]
 
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
@@ -51,8 +51,7 @@ def animate(frame):
    scatter.set_offsets(zip(boids[0], boids[1]))
 
 
-anim =  animation.FuncAnimation(figure, animate,
-                                frames=50, interval=50)
+anim =  animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     #plt.show()
--- test_boids.py
+++ test_boids.py
@@ -2,12 +2,14 @@ from boids import update_boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
+import numpy as np
 
 def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+    boid_data = regression_data["before"]
+    boid_data = [np.array(data) for data in boid_data]
     update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
-	
+    for after, before in zip(regression_data["after"], boid_data):
+        for after_value, before_value in zip(after, before):
+            assert_almost_equal(after_value, before_value, delta=0.01)
+
